f25597d5ccf2e365626e64a35e0ccd6f4c0d03fe
rahulrama <rahulrama@gmail.com>
2016-05-24 11:24:45 +0100

    Deleting restaurants

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -43,7 +43,6 @@ feature 'restaurants' do
      expect(page).to have_content 'KFC'
      expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
-
   end
 
   context 'editing restaurants' do
@@ -58,7 +57,18 @@ feature 'restaurants' do
       expect(page).to have_content 'Kentucky Fried Chicken'
       expect(current_path).to eq '/restaurants'
     end
+  end
 
-end
+  context 'deleting restaurants' do
+
+    before { Restaurant.create name: 'KFC' }
+
+    scenario 'removes a restaurant when a user clicks a delete link' do
+      visit '/restaurants'
+      click_link 'Delete KFC'
+      expect(page).not_to have_content 'KFC'
+      expect(page).to have_content 'Restaurant deleted successfully'
+    end
+  end
 
 end
