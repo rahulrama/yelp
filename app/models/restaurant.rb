eb0625b83abd50e6f94a42639fff1ee7acf60c74
rahulrama <rahulrama@gmail.com>
2016-05-25 18:04:31 +0100

    V3 complete

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,8 @@
 class Restaurant < ActiveRecord::Base
 
-  has_many :reviews, dependent: :destroy do
-    def build_with_user(attributes = {}, user)
-      attributes[:user] ||= user
-      build(attributes)
-    end
-  end
+  has_many :reviews,
+      -> { extending WithUserAssociationExtension },
+      dependent: :destroy
 
   belongs_to :user
 
