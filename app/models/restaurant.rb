5c2eb360ee3841621840c4e13f30d7c6ff94e271
rahulrama <rahulrama@gmail.com>
2016-05-30 09:39:45 +0100

    Bonus 5 completed, sans exercise

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,13 +5,13 @@ class Restaurant < ActiveRecord::Base
       dependent: :destroy
 
   belongs_to :user
-
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :name, length: { minimum: 3 }, uniqueness: true
 
   def average_rating
     return 'N/A' if reviews.none?
     reviews.average(:rating)
-  
   end
 
 end
